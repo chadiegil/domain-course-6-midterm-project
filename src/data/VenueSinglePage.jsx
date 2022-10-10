aae4938f621a1d62f82b6cae58aa2055396b7126
unknown <chadiegilaugis@gmail.com>
2022-10-10 15:02:14 +0800

    midterm

---
+++ src/data/VenueSinglePage.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+
+const VenueSinglePage = () => {
+  const { id } = useParams();
+  const [venue, setVenue] = useState([]);
+  const [schedule, setSchedule] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://sis.materdeicollege.com/api/venues/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setVenue(data.venue);
+        setSchedule(data.schedules[0]);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
+  console.log(schedule);
+  console.log(venue);
+
+  return (
+    <>
+      <div className="d-flex flex-column justify-content-center align-item-center">
+        <h1 className="text-center m-4">Mater Dei College {venue.building}</h1>
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Building</th>
+              <th scope="col">Capacity</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{venue.id}</td>
+              <td>{venue.name}</td>
+              <td>{venue.building}</td>
+              <td>{venue.capacity}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h1 className="text-center m-4">
+          {schedule ? "Schedules" : "No Schedule Found"}
+        </h1>
+        {schedule && (
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Course No.</th>
+                <th scope="col">Description</th>
+                <th scope="col">Schedule</th>
+                <th scope="col">Size</th>
+                <th scope="col">Teacher</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(schedule)?.map((sched, index) => {
+                return (
+                  <>
+                    <tr key={index}>
+                      <td>{schedule.id}</td>
+                      <td>{schedule.course_no}</td>
+                      <td>{schedule.description}</td>
+                      <td>{schedule.schedule}</td>
+                      <td>{schedule.size}</td>
+                      <td>{schedule.teacher}</td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+      <Link to="/" className="btn btn-sm btn-primary mt-1">
+        back to venues
+      </Link>
+    </>
+  );
+};
+
+export default VenueSinglePage;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Fetch from "../data/Fetch";
+const Dashboard = () => {
+  return (
+    <div>
+      <Fetch />
+    </div>
+  );
+};
+
+export default Dashboard;
